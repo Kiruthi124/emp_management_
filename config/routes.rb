54674c97e2bc7a5f6e6158920d4457ea1932b264
Kiruthi124 <41139749+Kiruthi124@users.noreply.github.com>
2018-09-04 10:02:51 -0400

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root :to => "employees#index"
+root to: 'employees#index'
   resources :employees
 end
