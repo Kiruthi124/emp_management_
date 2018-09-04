03c661868789d7a64ba6e52cfb3c98fdc0707a1e
Kiruthi124 <41139749+Kiruthi124@users.noreply.github.com>
2018-09-04 12:26:10 -0400

    Initial Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  rescue_from Timeout::Error, :with => :rescue_from_timeout
-
-  protected
-
-  def rescue_from_timeout(exception)
-    # code to handle the issue
-  end
+  
 end
